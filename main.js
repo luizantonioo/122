cafc34796cbd886d3849f85361348c91fdf45c72
luizantonioo <125517343+luizantonioo@users.noreply.github.com>
2023-10-18 16:54:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,21 +9,24 @@ video = createCapture(VIDEO);
 video.hide();
 tintColor = "";
 }
-
 function draw(){
-image(video, 0, 0, 640, 480)
-tint(tintcolor);
-circle(10,10);
-circle(10,480);
-circle(480,480);
-circle(10,480);
-rect(10,10);
-rect(480,480);
-rect(10,480);
-rect(480,10);
-ellipse(56, 46, 55, 55);
+    image(video, 0, 0, 640, 480)
+    tint(tintColor);  // Escrever igual na linha 10
+    fill(255, 0, 0);   // Colocar a cor do preenchimento
+    stroke(255, 0, 0);  // Colocar a cor da linha
+    circle(50, 50, 80);   // Comando circle, precisa colocar o x, y, raio
+    circle(50, 430, 80);
+    circle(590, 50, 80);
+    circle(590, 430, 80);
+    fill(0, 128, 0);   // Colocar a cor do preenchimento
+    stroke(0, 128, 0);  // Colocar a cor da linha
+    rect(90, 40, 460, 20);  // Comando rec, precisa colocar o x, y, largura e altura
+    rect(90, 420, 460, 20);
+    rect(40, 90, 20, 300);
+    rect(580, 90, 20, 300);
+    }
 
-}
+
 
 function filtro() {
 tintColor = document.getElementById("cor").value;
